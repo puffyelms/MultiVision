d85d329330546b2748d8fccf29c583e8ce072187
Tim Getz <timgetz@gmail.com>
2016-03-21 14:48:18 -0400

    Refactored server.js

--- public/app/app.js
+++ public/app/app.js
@@ -5,7 +5,7 @@
     angular.module('app').config(function ($routeProvider, $locationProvider) {
         // $locationProvider.html5Mode(true);
         $routeProvider
-            .when('/', {templateUrl: '/partials/main', controller: 'myMainCtrl'})
+            .when('/', {templateUrl: '/partials/main/main', controller: 'myMainCtrl'})
     });
 
 
--- server.js
+++ server.js
@@ -1,53 +1,15 @@
 var express = require('express'),
-    stylus = require('stylus'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-function compile(str, path) {
-    return stylus(str).set('filename', path);
-}
+var config = require('./server/config/config')[env];
 
-app.set('views', __dirname + '/server/views');
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(stylus.middleware(
-    {
-        src: __dirname + '/public',
-        compile: compile
-    }
-));
-app.use(express.static(__dirname + '/public'));
+require('./server/config/express')(app,config);
+require('./server/config/mongoose')(config);
+require('./server/config/routes')(app);
 
-mongoose.connect('mongodb://localhost/multivision');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error....'));
-db.once('open', function callback() {
-    console.log('multivision db opened');
-});
-
-// var messageSchema = mongoose.Schema({message: String});
-// var Message = mongoose.model('Message', messageSchema);
-// var mongoMessage;
-// Message.findOne().exec(function(err, messageDoc) {
-//     mongoMessage = messageDoc.message;
-// });
-
-app.get('/partials/:partialPath', function(req, res) {
-    res.render('partials/' + req.params.partialPath);
-});
-
-app.get('*', function (req, res) {
-    res.render('index');
-});
-
-var port = 3030;
-app.listen(port);
-
-console.log('Listening on port '+ port + '...');
+app.listen(config.port);
+console.log('Listening on port '+ config.port + '...');
