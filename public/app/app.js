636859aa9cdc307ea56f69143e74149a0c7585d1
Tim Getz <timgetz@gmail.com>
2016-04-18 20:51:00 -0400

    Added authorization and admin page.

--- public/app/app.js
+++ public/app/app.js
@@ -1,30 +1,26 @@
-(function() {
+angular.module('app', ['ngResource', 'ngRoute']);
 
-    angular.module('app', ['ngResource', 'ngRoute']);
+angular.module('app').config(function ($routeProvider, $locationProvider) {
 
-    angular.module('app').config(function ($routeProvider, $locationProvider) {
-
-        var routeRoleChecks = {
-            admin: {auth: function(mvAuth) {
-                return mvAuth.authorizeCurrentUserForRoute('admin');
-            }}
-        };
+    var routeRoleChecks = {
+        admin: {auth: function(mvAuth) {
+            return mvAuth.authorizeCurrentUserForRoute('admin');
+        }}
+    };
 
 
-        $locationProvider.html5Mode(true);
-        $routeProvider
-            .when('/', {templateUrl: '/partials/main/main', controller: 'myMainCtrl'})
-            .when('/admin/users', {templateUrl: '/partials/admin/user-list',
-                controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
-            });
-    });
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/', {templateUrl: '/partials/main/main', controller: 'myMainCtrl'})
+        .when('/admin/users', {templateUrl: '/partials/admin/user-list',
+            controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
+        });
+});
 
-    angular.module('app').run(function($rootScope, $location) {
-        $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
-            if(rejection === 'not authorized') {
-                $location.path('/');
-            }
-        })
+angular.module('app').run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
+        if(rejection === 'not authorized') {
+            $location.path('/');
+        }
     })
-
-}());
+})
